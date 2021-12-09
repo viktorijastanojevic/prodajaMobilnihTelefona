6584de1ad966533659e1bdaf763a371182f7e312
viktorijastanojevic <stanojevicviktorija@gmail.com>
2021-12-09 15:51:29 +0100

    dodata funkcija pretrazivanja po nazivu

--- index.php
+++ index.php
@@ -81,6 +81,7 @@ body {font-family: Arial, Helvetica, sans-serif;}
 }
 </style>
  <script src="js/vote.js"></script>
+ <script src="js/searchandsort.js"></script>
 <script src="https://kit.fontawesome.com/64d58efce2.js" crossorigin="anonymous"></script>
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
       <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
@@ -101,12 +102,14 @@ body {font-family: Arial, Helvetica, sans-serif;}
 
         <div class="mainPart" style="background-color: 	rgb(208,208,208,0.9) ;   border-radius: 5px; padding:30px; margin:10%" >
 
-                        <h2><i class="fa fa-mobile" aria-hidden="true"></i> Mobile phones</h2>
+                        <h2><i class="fa fa-mobile" aria-hidden="true"></i> Mobile phones</h2> 
+                       <input type="text" id="myInput" onkeyup="search()" placeholder="Search for names.." title="Type in a name" style="float:right;font-size:25px;border:none;
+    border-radius: 5px;">   <i class="fa fa-search" aria-hidden="true" style="float:right; font-size:30px;padding:4px"></i>
 
 
 
                         
-                        <table class="table table-hover"  style=" color:black; " >
+                        <table class="table table-hover"  style=" color:black; "  id="myTable">
                             <thead>
                                 <tr>
                                 <th scope="col">ID</th>
